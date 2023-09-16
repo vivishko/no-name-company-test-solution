1bb2ff24986a46fd12343ce08ebd01ca4d1e08ba
vivi <viloloshka8@gmail.com>
2023-09-17 00:26:49 +0300

    style: :art:

--- src/address/address.controller.ts
+++ src/address/address.controller.ts
@@ -1,9 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
+import { getLastBlockNumber, getBlockInfo } from '../helpers/fetchFunctions';
 
 @Controller('address')
 export class AddressController {
   @Get()
   getMostChanged(): string {
+    // запросить инфу о последних 100 блоках
+
+    // вычленить из них n транзакций
+
+    // выписать из каждой из них адреса кошельков - from и to - в мапу:
+    // если кошелёк в from - минусовать число, если в to - плюсовать
+
     return 'This action returns all cats';
   }
 }
